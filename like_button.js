6e7179cfe012659e36257898ed31f42dd2aa2bfa
Aliyarm <aliyar.m03@gmail.com>
2023-07-08 15:27:59 +0600

    first commit

---
+++ like_button.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      let num = document.querySelector('#tentacles').value;
+      let currency = document.querySelector('select').value;
+      let multiply = num * currency;
+      let result = document.querySelector('.result');
+      result.textContent = multiply;
+      console.log(multiply);
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
